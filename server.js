ccb1f39edce5df7e3b2da2284e5a03f1fd8cf1d1
Atul-Nambudiri <atul.nambudiri@gmail.com>
2015-02-28 01:39:40 -0600

    Switched to Express

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ var app = express()
 var port = process.env.PORT || 1337;
 
 // Serve static files
-app.use(express.static(path.join(__dirname, 'inkWaves'))); 
+app.use('/', express.static(path.join(__dirname + '/inkWaves')));
+app.get('/t', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+})
 
 // Route for everything else.
 app.get('*', function(req, res){
