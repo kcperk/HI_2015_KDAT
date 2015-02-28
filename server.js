0dc3dd6daee379f95a472361518823bf7f262b98
Atul-Nambudiri <atul.nambudiri@gmail.com>
2015-02-28 00:20:12 -0600

    Azure Test2

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 var http = require('http');
 
-
-
+var port = process.env.PORT || 1337;
 function handleRequest(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.write("Hello World");
   response.end();
 }
 
-http.createServer(handleRequest).listen(8888);
+http.createServer(handleRequest).listen(port);
 console.log("Server is up);
