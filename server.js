f6967a4b0ba64b5045f6bd99cbf6b47d6a71c971
Atul-Nambudiri <atul.nambudiri@gmail.com>
2015-02-28 00:25:01 -0600

    Hello

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ var http = require('http');
 var port = process.env.PORT || 1337;
 function handleRequest(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.end();
+  response.end("Hello World\n");
 }
 
 http.createServer(handleRequest).listen(port);
