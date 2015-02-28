f49e7eec1e80bc378041ec4dab3f2a36a6f8dc99
Atul-Nambudiri <atul.nambudiri@gmail.com>
2015-02-28 00:25:49 -0600

    Hello

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
-var http = require('http');
-
+var http = require('http')
 var port = process.env.PORT || 1337;
-function handleRequest(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World\n");
-}
-
-http.createServer(handleRequest).listen(port);
-console.log("Server is up);
+http.createServer(function(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World\n');
+}).listen(port);
