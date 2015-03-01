f42790fa0e5b6d6237ba945e07e4f054f9976c13
Atul-Nambudiri <atul.nambudiri@gmail.com>
2015-02-28 20:44:58 -0600

    Some more

--- server.js
+++ server.js
@@ -3,24 +3,21 @@ var mongoose = require('mongoose');
 var passport = require('passport');
 var session  = require('express-session');
 var bodyParser = require('body-parser');
+var flash    = require('connect-flash');
 
 
 var database = require('./db.js');
-
-var app      = express();
 mongoose.connect(database.url);
 
+var app      = express();
 var port     = process.env.PORT || 1337;
 
 //Setup Passport to save sessions
-app.use(session({
-  secret: 'sosecret',  // session secret
-  resave: false,
-  saveUninitialized: false}));
+app.use(session({ secret: 'sosecret', resave: false, saveUninitialized: false}));
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(bodyParser.urlencoded({ extended: false }));
-
+app.use(flash);
 
 
 require('./routes.js')(app, passport);
